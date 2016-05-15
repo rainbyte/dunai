3ef35ce89bb024d7a4afced5ee01d621d6e7b095
Ivan Perez <ivan.perez@keera.co.uk>
2016-05-15 13:33:05 +0100

    Comments, cleaning, modular defs.

--- examples/discrete/WXText.hs
+++ examples/discrete/WXText.hs
@@ -6,6 +6,8 @@ import Control.Monad
 import Data.IORef
 import Data.MonadicStreamFunction
 import Graphics.UI.WX
+import Control.Category
+import Prelude hiding ((.))
 
 main :: IO ()
 main = start hello
@@ -17,7 +19,15 @@ hello
        entry  <- textEntry  f []
        quit   <- button     f [text := "Quit", on command := close f]
 
-       hndlr  <- pushReactimate_ (myMSF lenLbl entry)
+       -- Reactive network
+       let appMSF =
+             labelTextSk lenLbl . arr (show.length) . textEntryTextSg entry
+
+       -- appMSF =
+       --   textEntryTextSg entry >>> arr (show.length) >>> labelTextSk lenLbl
+
+       hndlr <- pushReactimate_ appMSF 
+
        set entry [ on update := hndlr ]
        
        set f [layout := margin 10 (column 5 [ floatCentre (widget lenLbl)
@@ -25,10 +35,22 @@ hello
                                             , floatCentre (widget quit)
                                             ] )]
 
-myMSF lbl entry = liftMStreamF_ (get entry text)
-              >>> arr (show.length)
-              >>> liftMStreamF (\t -> set lbl [ text := t ])
 
+
+-- * Adhoc Dunai-WX backend
+
+textEntryTextSg :: TextCtrl a -> MStream IO String
+textEntryTextSg entry = liftMStreamF_ (get entry text)
+
+labelTextSk :: StaticText a -> MSink IO String
+labelTextSk lbl = liftMStreamF (\t -> set lbl [ text := t ])
+
+-- * Slightly general MSF definitions
+
+type MSink m a = MStreamF m a ()
+
+-- | Run an MSF on an input sample step by step, using an IORef to store the
+-- continuation.
 pushReactimate :: MStreamF IO a b -> IO (a -> IO b)
 pushReactimate msf = do
   msfRef <- newIORef msf
@@ -38,6 +60,8 @@ pushReactimate msf = do
               writeIORef msfRef msf''
               return b
 
+-- | Run one step of an MSF on () streams, internally storing the
+-- continuation.
 pushReactimate_ :: MStreamF IO () () -> IO (IO ())
 pushReactimate_ msf = do
   f <- pushReactimate msf
